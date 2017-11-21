7ef307406f795dc65382926f1ef98321b97d32cc
Zhe Chen <chenzhekl@gmail.com>
2017-11-21 19:37:36 +0900

    Clean up old code.

--- common/black_hole.js
+++ common/black_hole.js
@@ -15,48 +15,45 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-function string_starts_with (str, substr) {
-  'use strict'
+function stringStartsWith (str, substr) {
   return str.slice(0, substr.length) === substr
 }
 
-function _parse_url (url_str) {
-  'use strict'
-  var protocol = null
-  if (string_starts_with(url_str, 'http://')) {
-    url_str = url_str.slice('http://'.length)
+function parseUrl (urlStr) {
+  let protocol = null
+  if (stringStartsWith(urlStr, 'http://')) {
+    urlStr = urlStr.slice('http://'.length)
     protocol = 'http'
-  } else if (string_starts_with(url_str, 'https://')) {
-    url_str = url_str.slice('https://'.length)
+  } else if (stringStartsWith(urlStr, 'https://')) {
+    urlStr = urlStr.slice('https://'.length)
     protocol = 'https'
   } else {
     console.error('URL does not start with http:// or https://')
     return null
   }
 
-  var path_idx = url_str.indexOf('/')
-  if (path_idx < 0) {
-    path_idx = url_str.length
-    url_str += '/'
+  let pathIdx = urlStr.indexOf('/')
+  if (pathIdx < 0) {
+    pathIdx = urlStr.length
+    urlStr += '/'
   }
-  var colon_idx = url_str.indexOf(':')  // the colon before the optional port number
+  const colonIdx = urlStr.indexOf(':')
 
-  var sep_idx = path_idx
-  if (colon_idx >= 0 && colon_idx < path_idx) {
-    sep_idx = colon_idx
+  let sepIdx = pathIdx
+  if (colonIdx >= 0 && colonIdx < pathIdx) {
+    sepIdx = colonIdx
   }
 
   return {
     protocol: protocol,
     // the parameter in FindProxyForURL only doesn't contain port numbers
-    hostname: url_str.slice(0, sep_idx),
-    portpath: url_str.slice(sep_idx)
+    hostname: urlStr.slice(0, sepIdx),
+    portpath: urlStr.slice(sepIdx)
   }
 }
 
-function gen_url_map (protocol, white_ulist, proxy_ulist) {
-  'use strict'
-  var url_map = {
+function genUrlMap (protocol, whiteUlist, proxyUlist) {
+  const urlMap = {
     white: {
       any: []
     },
@@ -65,11 +62,11 @@ function gen_url_map (protocol, white_ulist, proxy_ulist) {
     }
   }
 
-  function add_patterns (map_obj, ulist) {
-    var i, uobj, hostname, portpath
-    var key, val
+  function addPatterns (mapObj, ulist) {
+    let i, uobj, hostname, portpath
+    let key, val
     for (i = 0; i < ulist.length; i++) {
-      uobj = _parse_url(ulist[i])
+      uobj = parseUrl(ulist[i])
       if (uobj === null) {
         console.error('Invalid URL pattern: ' + ulist[i])
         continue
@@ -79,24 +76,24 @@ function gen_url_map (protocol, white_ulist, proxy_ulist) {
         hostname = uobj.hostname
         portpath = uobj.portpath
         if (hostname.indexOf('*') >= 0) {
-          if (hostname.slice(1).indexOf('*') >= 0) {  // * is only allowed to be the first char
+          if (hostname.slice(1).indexOf('*') >= 0) {
             console.error('Invalid wildcard URL pattern: ' + ulist[i])
             continue
           } else {
             key = 'any'
-            val = hostname + portpath  // host:port/path
+            val = hostname + portpath
           }
         } else {
-          if (!map_obj.hasOwnProperty(hostname)) {
-            map_obj[hostname] = []
+          if (!mapObj.hasOwnProperty(hostname)) {
+            mapObj[hostname] = []
           }
           key = hostname
-          val = portpath  // only :port/path
+          val = portpath
         }
 
-        val = val.replace(/[\-\[\]\/\{\}\(\)\+\?\.\\\^\$\|]/g, '\\$&')
+        val = val.replace(/[-[\]/{}()+?.\\^$|]/g, '\\$&')
         val = val.replace(/\*/g, '.*')
-        val = val.replace(/^\.\*/i, '[^\/]*')  // if starts with *; should not be possible for :port or /path
+        val = val.replace(/^\.\*/i, '[^/]*')
 
         if (val.slice(-2) === '.*') {
           val = val.slice(0, -2)
@@ -105,97 +102,103 @@ function gen_url_map (protocol, white_ulist, proxy_ulist) {
           val = new RegExp('^' + val + '$', 'i')
         }
 
-        map_obj[key].push(val)
-      }  // if
-    }  // for
+        mapObj[key].push(val)
+      }
+    }
   }
 
-  add_patterns(url_map.white, white_ulist)
-  add_patterns(url_map.proxy, proxy_ulist)
+  addPatterns(urlMap.white, whiteUlist)
+  addPatterns(urlMap.proxy, proxyUlist)
 
-  return url_map
+  return urlMap
 }
 
-function urls2pac (url_whitelist, url_list,
-                   proxy_server_1, proxy_protocol_1,
-                   proxy_server_2, proxy_protocol_2) {
-  'use strict'
-
-  if (typeof proxy_protocol_1 === 'undefined') {
-    proxy_protocol_1 = 'PROXY'
+function urls2pac (urlWhitelist, urlList,
+  proxyServer1, proxyProtocol1,
+  proxyServer2, proxyProtocol2) {
+  if (typeof proxyProtocol1 === 'undefined') {
+    proxyProtocol1 = 'PROXY'
   } else {
-    proxy_protocol_1 = proxy_protocol_1.replace(/:/g, '')
-    proxy_protocol_1 = proxy_protocol_1.replace(/\//g, '')
-    proxy_protocol_1 = proxy_protocol_1.toUpperCase()
-    if (proxy_protocol_1 === 'HTTP') {
-      proxy_protocol_1 = 'PROXY'
+    proxyProtocol1 = proxyProtocol1.replace(/:/g, '')
+    proxyProtocol1 = proxyProtocol1.replace(/\//g, '')
+    proxyProtocol1 = proxyProtocol1.toUpperCase()
+    if (proxyProtocol1 === 'HTTP') {
+      proxyProtocol1 = 'PROXY'
     }
   }
 
-  var _proxy_str = proxy_protocol_1 + ' ' + proxy_server_1 + '; '
+  let proxyStr = proxyProtocol1 + ' ' + proxyServer1 + '; '
 
-  if (typeof proxy_server_2 !== 'undefined') {
-    if (typeof proxy_protocol_2 === 'undefined') {
-      proxy_protocol_2 = 'PROXY'
+  if (typeof proxyServer2 !== 'undefined') {
+    if (typeof proxyProtocol2 === 'undefined') {
+      proxyProtocol2 = 'PROXY'
     }
-    proxy_protocol_2 = proxy_protocol_2.replace(/:/g, '')
-    proxy_protocol_2 = proxy_protocol_2.replace(/\//g, '')
-    proxy_protocol_2 = proxy_protocol_2.toUpperCase()
-    if (proxy_protocol_2 === 'HTTP') {
-      proxy_protocol_2 = 'PROXY'
+    proxyProtocol2 = proxyProtocol2.replace(/:/g, '')
+    proxyProtocol2 = proxyProtocol2.replace(/\//g, '')
+    proxyProtocol2 = proxyProtocol2.toUpperCase()
+    if (proxyProtocol2 === 'HTTP') {
+      proxyProtocol2 = 'PROXY'
     }
 
-    _proxy_str += proxy_protocol_2 + ' ' + proxy_server_2 + '; '
+    proxyStr += proxyProtocol2 + ' ' + proxyServer2 + '; '
   }
 
-  _proxy_str += 'DIRECT'
+  proxyStr += 'DIRECT'
 
-  var _http_map = gen_url_map('http', url_whitelist, url_list)
-  var _https_map = gen_url_map('https', url_whitelist, url_list)
-  var _proxy_str = _proxy_str
+  const httpMap = genUrlMap('http', urlWhitelist, urlList)
+  const httpsMap = genUrlMap('https', urlWhitelist, urlList)
 
-  function _check_regex_list (regex_list, str) {
+  function checkRegexList (regexList, str) {
     if (str.slice(0, 4) === ':80/') { str = str.slice(3) }
-    for (var i = 0; i < regex_list.length; i++) {
-      if (regex_list[i].test(str)) { return true }
+    for (let i = 0; i < regexList.length; i++) {
+      if (regexList[i].test(str)) { return true }
     }
     return false
   }
 
-  function _check_patterns (patterns, hostname, full_url, prot_len) {
+  function checkPatterns (patterns, hostname, fullUrl, protoLen) {
     if (patterns.hasOwnProperty(hostname)) {
-      if (_check_regex_list(patterns[hostname],
-          full_url.slice(prot_len + hostname.length)))  // check only :port/path
-      { return true }
+      if (checkRegexList(patterns[hostname], fullUrl.slice(protoLen + hostname.length))) {
+        return true
+      }
     }
-    if (_check_regex_list(patterns.any,  // try our best to speed up the checking for non-proxied urls
-        full_url.slice(prot_len)))  // check hostname:port/path
-    { return true }
+    if (checkRegexList(patterns.any, fullUrl.slice(protoLen))) {
+      return true
+    }
     return false
   }
 
-  function _find_proxy (url_map, host, url, prot_len) {
-    if (_check_patterns(url_map.white, host, url, prot_len)) { return 'DIRECT' }
-    if (_check_patterns(url_map.proxy, host, url, prot_len)) { return _proxy_str }
+  function findProxy (urlMap, host, url, protoLen) {
+    if (checkPatterns(urlMap.white, host, url, protoLen)) {
+      return 'DIRECT'
+    }
+    if (checkPatterns(urlMap.proxy, host, url, protoLen)) {
+      return proxyStr
+    }
     return 'DIRECT'
   }
 
-  function FindProxyForURL (url, host) {  // host doesn't contain port
-    var prot = url.slice(0, 6)
-    if (prot === 'http:/') { return _find_proxy(_http_map, host, url, 7) }  // 'http://'.length
-    else if (prot === 'https:') { return _find_proxy(_https_map, host, url, 8) }  // 'https://'.length
+  function findProxyForURL (url, host) {
+    const prot = url.slice(0, 6)
+    if (prot === 'http:/') {
+      return findProxy(httpMap, host, url, 7)
+    } else if (prot === 'https:') {
+      return findProxy(httpsMap, host, url, 8)
+    }
     return 'DIRECT'
   }
 
-  return FindProxyForURL
+  return findProxyForURL
 }
 
 const findProxyForURL = urls2pac(
+  // eslint-disable-next-line
   urlDb.proxyBypassURLs, urlDb.redirectURLs.concat(urlDb.proxyURLs),
   'secure.uku.im:8443', 'HTTPS',
   'secure.uku.im:993', 'HTTPS'
 )
 
+// eslint-disable-next-line
 function FindProxyForURL (url, host) {
   return findProxyForURL(`${url}/`, host)
 }
