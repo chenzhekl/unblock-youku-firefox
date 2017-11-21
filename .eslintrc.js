70e67be3be40c4c1e1f1846ed80851ab9cf3def6
Zhe Chen <chenzhekl@gmail.com>
2017-11-21 18:54:58 +0900

    switch from standard to eslint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true,
+    "webextensions": true
+  },
+  "extends": "standard",
+  "plugins": [
+    "standard",
+    "promise"
+  ]
+};
